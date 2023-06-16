995a758fd4827760f758975f8c1d6eadc2798f13
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-06-16 09:55:25 +0530

    create a react app with vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <p className="read-the-docs">BiteSpeed</p>
+    </>
+  );
+};
+
+export default App;
