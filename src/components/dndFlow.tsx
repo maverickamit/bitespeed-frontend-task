49288880b6bff597a3c52b79588d9739f7e941a9
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-06-19 16:27:26 +0530

    implement useDrop hook to display nodes on drop

--- src/components/dndFlow.tsx
+++ src/components/dndFlow.tsx
@@ -7,7 +7,9 @@ import ReactFlow, {
   Controls,
   Edge,
   Connection,
+  ReactFlowInstance,
 } from "reactflow";
+import { useDrop } from "react-dnd";
 import "reactflow/dist/style.css";
 import "./dndFlow.css";
 
@@ -20,19 +22,49 @@ const initialNodes = [
   },
 ];
 
-const DndFlow: React.FC = () => {
+const DnDFlow: React.FC = () => {
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+  const [reactFlowInstance, setReactFlowInstance] =
+    useState<ReactFlowInstance | null>(null);
+  const [numOfNodes, setNumOfNodes] = useState(1);
 
   const onConnect = useCallback(
     (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
 
+  const [, drop] = useDrop(
+    () => ({
+      accept: "message",
+      drop: (_, monitor) => {
+        const num = numOfNodes + 1;
+        const delta = monitor.getSourceClientOffset() as XYCoord;
+        const reactFlowBounds =
+          reactFlowWrapper.current.getBoundingClientRect();
+
+        const position = reactFlowInstance.project({
+          x: delta.x - reactFlowBounds.left,
+          y: delta.y - reactFlowBounds.top,
+        });
+        setNodes((prevNodes) =>
+          prevNodes.concat({
+            id: num.toString(10),
+            position,
+            type: "default",
+            data: { label: "default node " + num.toString(10) },
+            draggable: true,
+          })
+        );
+        setNumOfNodes((prevNum) => prevNum + 1);
+      },
+    }),
+    [numOfNodes, reactFlowInstance]
+  );
+
   return (
-    <div className="dnd-flow">
+    <div className="dnd-flow" ref={drop}>
       <div className="reactflow-wrapper" ref={reactFlowWrapper}>
         <ReactFlowProvider>
           <ReactFlow
@@ -51,4 +83,4 @@ const DndFlow: React.FC = () => {
   );
 };
 
-export default DndFlow;
+export default DnDFlow;
