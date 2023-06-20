bc41ac067942619a5e73d79235fda2614cf3c2ec
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-06-20 19:11:27 +0530

    add back button on settings panel to go back to nodes panel - click on back button to go back to nodes panel

--- src/components/messageInputSettings.tsx
+++ src/components/messageInputSettings.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text } from "@mantine/core";
-import "./messageInputSettings.css";
 import { useState } from "react";
+import { ArrowLeft } from "tabler-icons-react";
+import { SidePanelContext } from "./dashboard";
+import "./messageInputSettings.css";
 
 const MessageInputSettings: React.FC = () => {
   const [message, setMessage] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
   };
+  const sidePanelContextValue = useContext(SidePanelContext);
 
   return (
     <div className="message-input-settings">
-      <Text className="header">Message</Text>
+      <div className="header">
+        <ArrowLeft
+          className="left-arrow"
+          size={30}
+          onClick={() => sidePanelContextValue.setSettingsPanelOpen(false)}
+        />
+        <Text>Message</Text>
+      </div>
       <div className="text-input">
         <textarea value={message} onChange={handleChange} />
       </div>
